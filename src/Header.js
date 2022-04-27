1cc086e1a64caf49c127185fa2c3aeaded2afdb3
Eduardo Oliveira <eos.eduardooliveira@gmail.com>
2022-04-27 03:01:39 -0300

    create home page

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,6 @@
 import SearchIcon from "@material-ui/icons/Search";
 import React from "react";
-import "./header.css";
+import "./Header.css";
 import { Link } from "react-router-dom";
 import AddShoppingCartOutlinedIcon from "@material-ui/icons/AddShoppingCartOutlined";
 
@@ -8,24 +8,24 @@ function Header() {
   return (
     <nav className="header">
       <img
-        className="header__logo"
+        className="header-logo"
         alt="logo"
         src="https://liven.tech/_next/image?url=%2Fimages%2Flogo-2x.png&w=256&q=75"
       />
-      <div className="header__search">
-        <input type="text" className="header__search" />
-        <SearchIcon className="header__searchIcon" />
+      <div className="header-search">
+        <input type="text" className="header-search" />
+        <SearchIcon className="header-searchIcon" />
       </div>
-      <Link to="/" className="header__link">
-        <div className="header__option">
-          <span className="header__optionLineOne">Olá, Usuário</span>
-          <span className="header__optionLineTwo">Sign in or Sign Out</span>
+      <Link to="/" className="header-link">
+        <div className="header-option">
+          <span className="header-optionLineOne">Olá, Usuário</span>
+          <span className="header-optionLineTwo">Sign in or Sign Out</span>
         </div>
       </Link>
-      <Link to="/checkout" className="header__link">
-        <div className="header__optionCart">
+      <Link to="/checkout" className="header-link">
+        <div className="header-optionCart">
           <AddShoppingCartOutlinedIcon />
-          <span className="header__optionLineTwo header__cartCount">8</span>
+          <span className="header-optionLineTwo header-cartCount">8</span>
         </div>
       </Link>
     </nav>
--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,88 @@
+import React from "react";
+import Product from "./Product";
+import "./Home.css";
+
 function Home() {
   return (
     <div className="home">
-      <img src=""
+      <img
+        className="home-image"
+        src="https://m.media-amazon.com/images/I/61J7RPieVOL._SX1500_.jpg"
+        alt=""
+      />
+
+      <div className="home-row">
+        <Product
+          id="12345"
+          title="Amazfit gtr 3"
+          price={1360.99}
+          rating={4}
+          image="https://m.media-amazon.com/images/I/61h2jznRbyL._AC_SL1500_.jpg"
+        />
+        <Product
+          id="12346"
+          title="Ticwatch E3 Smart Watch"
+          price={1098.0}
+          rating={3}
+          image="https://m.media-amazon.com/images/I/61p3TcCeRUS._AC_SX522_.jpg"
+        />
+        <Product
+          id="12347"
+          title="Relógio Smartwatch Amazfit GTS 2"
+          price={579.0}
+          rating={5}
+          image="https://m.media-amazon.com/images/I/61hmDN6921L._AC_SL1500_.jpg"
+        />
+      </div>
+
+      <div className="home-row">
+        <Product
+          id="12345"
+          title="Amazfit gtr 3"
+          price={1360.99}
+          rating={4}
+          image="https://m.media-amazon.com/images/I/61h2jznRbyL._AC_SL1500_.jpg"
+        />
+        <Product
+          id="12346"
+          title="Ticwatch E3 Smart Watch"
+          price={1098.0}
+          rating={3}
+          image="https://m.media-amazon.com/images/I/61p3TcCeRUS._AC_SX522_.jpg"
+        />
+        <Product
+          id="12347"
+          title="Relógio Smartwatch Amazfit GTS 2"
+          price={579.0}
+          rating={5}
+          image="https://m.media-amazon.com/images/I/61hmDN6921L._AC_SL1500_.jpg"
+        />
+        <Product
+          id="12345"
+          title="Amazfit gtr 3"
+          price={1360.99}
+          rating={4}
+          image="https://m.media-amazon.com/images/I/61h2jznRbyL._AC_SL1500_.jpg"
+        />
+      </div>
+      <div className="home-row">
+        <Product
+          id="12346"
+          title="Ticwatch E3 Smart Watch"
+          price={1098.0}
+          rating={3}
+          image="https://m.media-amazon.com/images/I/61p3TcCeRUS._AC_SX522_.jpg"
+        />
+        <Product
+          id="12347"
+          title="Relógio Smartwatch Amazfit GTS 2"
+          price={579.0}
+          rating={5}
+          image="https://m.media-amazon.com/images/I/61hmDN6921L._AC_SL1500_.jpg"
+        />
+      </div>
     </div>
-    )
+  );
 }
 
 export default Home;
